d8e03caa8043ced2b90a6b686d28e405381552fd
tanishkave <tanishka.ve@gmail.com>
2019-02-08 07:54:13 +0530

    Added array programs

---
+++ C/Assignment 3/p1_subarrsum.c
@@ -0,0 +1,42 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int arr[20];
+	int N,sum,i,j;
+	int sum1=0,flag = 0;
+
+	printf("Enter number of elements: ");
+	scanf("%d",&N);
+
+	printf("Enter sum: ");
+	scanf("%d",&sum);
+
+	printf("Enter %d non-negative elements: ",N);
+
+	for(i=0;i<N;i++)
+		scanf("%d",&arr[i]);
+
+	for(i=0;i<N;i++)
+	{	
+		sum1=0;
+		for(j=i+1;j<=N;j++)
+		{
+			sum1=sum1+arr[j-1];
+			if(sum1>sum)
+			 break;
+			if(sum1==sum)
+			{   
+				flag=1;
+				printf("Sum found between indices: %d and %d\n",i,j-1);
+				break;
+			}
+		}
+		if(flag==1)
+			break;
+	}
+	if(flag==0)
+		printf("Subarray not found\n");
+
+	return 0;
+}
---
+++ C/Assignment 3/p2_negposmove.c
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	
+	int arr[10];
+	int i,j,temp;
+	
+	printf("Enter 10 integers: ");
+	
+	for(i=0;i<10;i++)
+		scanf("%d",&arr[i]);
+
+	for(i=0;i<=10;i++)
+	{
+		for(j=i+1;j<10;j++)
+		{
+			if(arr[j]<0&&0<=arr[i])
+			{
+				temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
+			}
+		}
+	
+	}
+
+	printf("Negative and Positive separated: \n");
+	for(i=0;i<10;i++)
+		printf("%d  ",arr[i]);
+	
+	return 0;
+}
