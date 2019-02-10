d687a41f0f9e80d9bf826e936919f3c8ad5af832
tanishkave <tanishka.ve@gmail.com>
2019-02-11 04:51:08 +0530

    Added Programs

---
+++ C/Assignment 4/p11_strtok.c
@@ -0,0 +1,66 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+char * strtok(char * s, char *comp);
+void main()
+{
+	char s[100], *p, delimit[20];
+	int i = 0, len = 0;
+	printf("Enter the input string \n");
+	gets(s);
+	printf("Enter the delimiter string \n");
+	gets(delimit);
+
+	while (len++ != '\0');
+	p = strtok(s, delimit);
+	while (p != NULL)
+	{
+		printf("%s \n", p);
+		p = strtok(NULL, delimit);
+	}
+}
+char * strtok(char * str, char *comp)
+{
+	static int pos;
+	static char *s;
+	int i = 0, start = pos;
+	if (str != NULL)
+		s = str;
+
+	i = 0;
+	int j = 0;
+
+	while (s[pos] != '\0')
+	{
+		j = 0;
+	
+		while (comp[j] != '\0')
+		{
+			
+			if (s[pos] == comp[j])
+			{
+				
+				s[pos] = '\0';
+				pos = pos + 1;
+				
+				if (s[start] != '\0')
+					return (&s[start]);
+				else
+				{
+				
+					start = pos;
+					pos--;
+					break;
+				}
+			}
+			j++;
+		}
+		pos++;
+	}
+	s[pos] = '\0';
+	if (s[start] == '\0')
+		return NULL;
+	else
+		return &s[start];
+}
+
+//checkk
