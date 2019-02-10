5cd6ec6e8edcccd0be51e53716078d980d22c7b8
tanishkave <tanishka.ve@gmail.com>
2019-02-11 04:31:32 +0530

    added programs

---
+++ C/Assignment 1/p12_rotateint.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int rotate_right(int, int);
+
+void main()
+{
+	int n,b;
+	
+	printf("Enter the number: ");
+	scanf("%d", &n);
+	
+	printf("Enter number of bits to be rotated: ");
+	scanf("%d", &b);
+
+	printf("%d rotated by %d bits = %d",n,b,rotate_right(n,b));
+}
+int rotate_right(int n, int b)
+{
+	int q,r,res;
+	r=n%100;
+	q=n/100;
+	res=(r*pow(10,b))+q;
+	return res;
+}
---
+++ C/Assignment 1/p13_invertselectbits.c
@@ -0,0 +1,41 @@
+﻿#include<stdio.h>
+
+int invertponwards(int,int,int);
+
+int main()
+{
+	int n,p,b;
+	
+	printf("Enter integer: ");
+	scanf("%d",&n);
+		
+	printf("Enter invert begin position: ");
+	scanf("%d",&p);
+	
+	printf("Enter number of bits to invert: ");
+	scanf("%d",&b);
+
+	printf("%d inverted by %d bits starting from %d position = %d",n,b,p,invertponwards(n,p,b));
+	
+	return 0;
+}
+
+int invertponwards(int n,int p,int b)
+{
+	int num = ((1 << (b+p)) - 1) ^ ((1 << (p - 1)) - 1);
+	return n^num;
+}
+
+
+/*
+C:\Users\tanis\Desktop\cdac_cprog\Assignment 1>p13_invertselectbits
+Enter integer: 50
+Enter invert begin position: 2
+Enter number of bits to invert: 3
+50 inverted by 3 bits starting from 2 position = 44
+
+50 = 110010
+44 = 101100
+
+
+*/
