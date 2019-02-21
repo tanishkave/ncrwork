ab78ae379ca1286e2bacb73a9bcdac0957b1eeb1
tanishkave <tanishka.ve@gmail.com>
2019-02-21 06:57:02 +0530

    Added program 8

---
+++ C++/Assignment 1/p8_constdest_invoc.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+using namespace std;
+
+//order of invocation of constructors and destructors
+
+class Employee
+{
+	int empno;
+	int salary;
+
+	public:
+	
+	Employee()
+	{
+		empno=salary=0;
+		cout<<endl<<"Default Constructor"<<endl;
+	}
+
+	Employee(int x)
+	{
+		empno=salary=x;
+		cout<<endl<<"1 arg Constructor"<<endl;
+	}
+
+	Employee(int e,int s)
+	{
+		empno=e;
+		salary=s;
+		cout<<endl<<"2 args Constructor"<<endl;
+	}
+
+	Employee(Employee &e)
+	{
+		empno=e.empno;
+		salary=e.salary;
+		cout<<endl<<"Copy Constructor"<<endl;
+	}	
+	
+	~Employee()
+	{
+		cout<<endl<<"Destructor"<<endl;
+	}
+	
+};
+
+int main()
+{
+	Employee e1,e2(2,3),e3(7);	//Call by value;
+	Employee e4(e2);		//Call by reference
+	
+	cout<<endl<<"sizeof(Employee) = "<<sizeof(struct Employee)<<endl;
+	
+	return 0;
+
+
+}
